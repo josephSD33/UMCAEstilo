7906ae721679d1f4ff1582532a0dca25aebe4682
josephSD33 <136411783+josephSD33@users.noreply.github.com>
2026-02-04 20:58:04 -0600

    Creación de archivos base (Hoja de estilos, formulario, clase)

---
+++ Clases/CategoriaClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UMCAEstilo.Clases
+{
+    public class CategoriaClass
+    {
+    }
+}
---
+++ Vistas/CategoriaForm.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UMCAEstilo.Vistas
+{
+    public partial class CategoriaForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
